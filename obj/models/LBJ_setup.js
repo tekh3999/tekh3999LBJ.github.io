5c83ca7dcbed91f4e53739f4226d087d8bd44c0d
tekh3999 <tekh4999@mail.com>
2019-07-17 11:02:08 -0600

    update
    change models

---
+++ obj/models/LBJ_setup.js
@@ -0,0 +1,24 @@
+{
+
+"metadata":
+{
+"sourceFile": "LBJ_setup.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 2955,
+"normals": 5724,
+"colors": 0,
+"uvs": 526,
+"triangles": 5724,
+"materials": 2
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "Wood_Walnut",
+"colorDiffuse"  : [0.6980, 0.3373, 0.1961],
+"colorAmbient"  : [0.1791, 0.0654, 0.0654],
+"colorSpecular"  : [0.9000, 0.9000, 0.9000],
+"transparency"  : 1.0,
+"specularCoef"  : 0.0,
